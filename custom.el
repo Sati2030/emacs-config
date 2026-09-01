57669298326e3b161848363fd45e9fe5b7012aec
Seiji Sato <seigifabian@gmail.com>
2026-09-01 10:09:15 +1000

    Initial commit of emacs config

---
+++ custom.el
@@ -0,0 +1,24 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-show-quick-access t nil nil "Numbers are helpful.")
+ '(custom-safe-themes
+   '("7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     default))
+ '(package-selected-packages '(claude-code-ide docker posframe))
+ '(package-vc-selected-packages
+   '((claude-code-ide :url
+		      "https://github.com/manzaltu/claude-code-ide.el")
+     (claudemacs :url "https://github.com/cpoile/claudemacs")
+     (grid :vc-backend Git :url
+	   "https://github.com/ichernyshovvv/grid.el" :branch "master"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
